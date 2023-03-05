c7cd6f296d54df4b894679b30cca027803877114
Joe DF <3848219+joedf@users.noreply.github.com>
2023-03-05 13:06:28 -0500

    #4 - NRMSE size tolerance, fix warn/error

--- app/src/img_metrics.js
+++ app/src/img_metrics.js
@@ -6,11 +6,25 @@
  */
 // eslint-disable-next-line no-unused-vars
 const NRMSE = {
-	compare: function(image1, image2) {
+	compare: function(image1, image2, tolerance) {
 		'use strict';
 
-		if (image1.data.length != image2.data.length)
-			throw "The given images have different data length or sizes.";
+		// size check and tolerance
+		if (image1.data.length != image2.data.length) {
+			let dl1 = image1.data.length, dl2 = image2.data.length;
+			var errmsg = "The given images have different data length ("
+				+image1.width+"x"+image1.height+" vs "+image2.width+"x"+image2.height
+				+") or sizes ("+dl1+" vs "+dl2+").";
+			
+			// get the tolerance or use default if not provided
+			tolerance = (typeof tolerance !== 'undefined') ? tolerance : 0.01;
+			let relDiff = Math.abs(dl1 - dl2) / Math.max(dl1, dl2);
+			if (relDiff <= tolerance) {
+				console.warn(errmsg + " Tolerance = "+tolerance+" relDiff = "+relDiff.toFixed(6));
+			} else {
+				throw errmsg;
+			}
+		}
 
 		const n_channels = 4; // assume grayscale RGBA flat array at 8 bit-depth (255)
 		const p_max = 255; // max pixel value for the bit depth
@@ -18,7 +32,19 @@ const NRMSE = {
 		// Do sum of squared difference
 		var sum = 0, len = image1.data.length;
 		for (var i = 0; i < len; i += n_channels) {
-			sum += Math.pow(image1.data[i] - image2.data[i], 2);
+			let diff = image1.data[i] - image2.data[i];
+
+			// check and allow for tolerance of diff image sizes
+			if (isNaN(diff)) {
+				if (i >= image2.data.length) {
+					console.warn("Calculation loop ended early as i = "+i);
+					break;
+				} else {
+					throw "Error: untolerated or invalid calculation!";
+				}
+			}
+
+			sum += Math.pow(diff, 2);
 		}
 
 		// number of pixels, each having multiple "channels",
