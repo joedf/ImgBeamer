1b8c7992764b455cc36e494bab7a11d98b4fc223
Joe DF <3848219+joedf@users.noreply.github.com>
2023-03-22 17:08:13 -0400

    jsdoc script - add clean option

--- jsdoc_build.bat
+++ jsdoc_build.bat
@@ -3,13 +3,28 @@
 @REM Set this to location of where you want the jsdoc files to be outputted.
 set JSDOC_OUTDIR=jsdocs
 
+
+SET arg1=%1
+
+IF /I _%arg1%_==_clean_ (
+	goto clean
+) ELSE (
+	call :clean
+	goto build
+)
+
+:clean
 echo clearing %JSDOC_OUTDIR% folder ...
 rmdir /s /q "%JSDOC_OUTDIR%"
+echo Cleared.
+goto:eof
+
+:build
 echo running jsdoc ...
 call jsdoc -d "%JSDOC_OUTDIR%" -c .jsdoc.json
-echo.
 echo Copy over the missing images for the readme ...
 xcopy app\src\img\icon.svg "%JSDOC_OUTDIR%\app\src\img\icon.svg*" /Y
 xcopy misc\screenshot3.png "%JSDOC_OUTDIR%\misc\screenshot3.png*" /Y
 echo.
 echo Done.
+goto:eof
