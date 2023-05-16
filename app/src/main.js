fc8ab7d292f9ffddb1392b94a225ce6b52dce225
Joe DF <joedf@live.ca>
2023-05-16 18:48:15 -0400

    add vSEM mode warning sub-resampled view

--- app/src/main.js
+++ app/src/main.js
@@ -218,6 +218,22 @@ function OnImageLoaded(eImg, stages){
 		spotScaling,
 		resampledBeam
 	);
+
+	// add Row-by-row / vSEM mode warning
+	var resampled_vSEM_note = Utils.ensureInfoBox(
+		resampledStage, 'vsem_mode',
+		function(){ alert($(this).attr('title'));
+	});
+	if (resampled_vSEM_note) {
+		// warn icon from GitHub's octicons (https://github.com/primer/octicons)
+		// eslint-disable-next-line max-len
+		const warn = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill="#FFFF00" d="M6.457 1.047c.659-1.234 2.427-1.234 3.086 0l6.082 11.378A1.75 1.75 0 0 1 14.082 15H1.918a1.75 1.75 0 0 1-1.543-2.575Zm1.763.707a.25.25 0 0 0-.44 0L1.698 13.132a.25.25 0 0 0 .22.368h12.164a.25.25 0 0 0 .22-.368Zm.53 3.996v2.5a.75.75 0 0 1-1.5 0v-2.5a.75.75 0 0 1 1.5 0ZM9 11a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"></path></svg>';
+		resampled_vSEM_note.innerHTML = warn;
+		resampled_vSEM_note.title = "For higher pixel counts, the drawing is done "
+			+ "row-by-row instead of frame-by-frame "
+			+ "for improved performance / responsiveness.";
+		$(resampled_vSEM_note).hide();
+	}
 	
 	var updateResamplingSteps = function(){
 		updateProbeLayout();
@@ -227,6 +243,10 @@ function OnImageLoaded(eImg, stages){
 		if ($(resampledStage.getContainer()).is(':visible')) {
 			updateResampled();
 		}
+
+		var rows = Utils.getRowsInput(), cols = Utils.getColsInput();
+		var warnVSEM = (rows*cols > G_AUTO_PREVIEW_LIMIT);
+		$(resampled_vSEM_note).toggle(warnVSEM);
 	};
 	G_UpdateResampled = updateResamplingSteps;
 
