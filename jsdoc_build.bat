66aba408d2a4becfbc7c83046e57284bd904d7f9
Joe DF <3848219+joedf@users.noreply.github.com>
2023-05-17 13:07:51 -0400

    jsdoc fix screenshot

--- jsdoc_build.bat
+++ jsdoc_build.bat
@@ -24,7 +24,7 @@ echo running jsdoc ...
 call jsdoc -d "%JSDOC_OUTDIR%" -c .jsdoc.json
 echo Copy over the missing images for the readme ...
 xcopy app\src\img\icon.svg "%JSDOC_OUTDIR%\app\src\img\icon.svg*" /Y
-xcopy misc\screenshot3.png "%JSDOC_OUTDIR%\misc\screenshot3.png*" /Y
+xcopy misc\screenshot4.png "%JSDOC_OUTDIR%\misc\screenshot*.png*" /Y
 echo.
 echo Done.
 goto:eof
