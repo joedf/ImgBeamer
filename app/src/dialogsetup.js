48b12a0e5288c25e89a1b099b0d6991cb54e7e46
Joe DF <3848219+joedf@users.noreply.github.com>
2025-03-31 23:40:15 -0400

    move dialog setup to a js file

---
+++ app/src/dialogsetup.js
@@ -0,0 +1,108 @@
+const G_STAGE_SIZE = GetOptimalBoxWidth();
+const G_BOXES_PER_ROW = 3;
+let g_dlg_selector = '.stage-dlg';
+
+$(g_dlg_selector).dialog({
+	maxHeight: 800,
+	maxWidth: 800,
+	minHeight: 200,
+	minWidth: 200,
+	width: G_STAGE_SIZE,
+	height: G_STAGE_SIZE + 27.6333 - 3.767,
+	resizable: false,
+	classes: {
+		"ui-dialog": "stage-dialog",
+	},
+	drag: function( event, ui ) {
+		// https://stackoverflow.com/a/20712561/883015
+		var snapTolerance = (G_STAGE_SIZE/10) + 0.2; //$(this).draggable('option', 'snapTolerance');
+		var grid = {
+			x: (G_STAGE_SIZE/10) + 0.2,
+			y: (G_STAGE_SIZE + 27.6333 - 3.767) / 10,
+		};
+
+		var topRemainder = ui.position.top % grid.y;
+		var leftRemainder = ui.position.left % grid.x;
+
+		if (topRemainder <= snapTolerance) {
+			ui.position.top = ui.position.top - topRemainder;
+		}
+
+		if (leftRemainder <= snapTolerance) {
+			ui.position.left = ui.position.left - leftRemainder;
+		}
+	},
+	resize: function( event, ui ) {
+		// https://stackoverflow.com/a/20712561/883015
+		var snapTolerance = 80; //$(this).draggable('option', 'snapTolerance');
+		var grid = {
+			x: 20,
+			y: 20,
+		};
+
+		var widthRemainder = ui.size.width % grid.x;
+		var heightRemainder = ui.size.height % grid.y;
+		
+		if (widthRemainder <= snapTolerance) {
+			ui.size.width = ui.size.width - widthRemainder;
+		}
+
+		if (heightRemainder <= snapTolerance) {
+			ui.size.height = ui.size.height - heightRemainder;
+		}
+	}
+}).dialogExtend({
+	"closable" : false,
+	"maximizable" : false,
+	"minimizable" : true,
+	"collapsable" : true,
+	"dblclick" : "collapse",
+	"minimizeLocation" : "right",
+	"icons": {
+		"collapse": "ui-icon-arrowthickstop-1-n"
+	},
+	"load": function(evt, dlg){
+		var e = $(this);
+		if (e.hasClass('dlg-start-minimized')) {
+			e.dialogExtend('minimize');
+		}
+	}
+});
+
+// tile dialogs
+// position first one
+$(g_dlg_selector).dialog('widget').eq(0).css({top:0, left:0});
+
+// tile the first 6 dialogs
+let dialogs = $(g_dlg_selector).dialog();
+let dialogs_count = 6; // dialogs.length;
+for (let i = 1; i < dialogs_count; i++) {
+	const dialog = dialogs.eq(i);
+	const prev = dialogs.eq(i-1).dialog('widget');
+
+	if (i % G_BOXES_PER_ROW == 0) {
+		const prev = dialogs.eq(i-G_BOXES_PER_ROW).dialog('widget');
+		var eDialog = prev.get(0);
+		var newPos = {
+			x: parseInt(prev.css('left')),
+			y: eDialog.offsetHeight + parseInt(prev.css('top')),
+		};
+		dialog.dialog('widget').css({top:newPos.y, left:newPos.x})
+	} else {
+		dialog.dialog({position: {my:"left top", at:"right top", of:$(prev)}});
+	}
+}
+
+// cascade the last 3 dialogs
+let cascade_start = 6;
+$(g_dlg_selector).eq(cascade_start).dialog({position: {my:"right top", at:"right top", of:'body'}});
+for (let i = 7; i < dialogs.length; i++) {
+	const base_z_index = 1000;
+	const dialog = dialogs.eq(i);
+	const prev = dialogs.eq(i-1).dialog('widget');
+	const n = i - cascade_start;
+	dialog.dialog('widget').css({
+		top:80*n, right:80*n, left:'auto',
+		'z-index':parseInt(prev.css('z-index'))+base_z_index + 1
+	});
+}
